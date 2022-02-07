358fb7d74cc68aeccc4bb9c19afa446eaa1ebedb
hengaoxing233 <76994928+hengaoxing233@users.noreply.github.com>
2022-02-07 17:32:45 +0800

    Create 14_T_Import_2.sol

---
+++ 14_T_Import_2.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./14_T_Import_1.sol";
+//import "https://github.com/hengaoxing233/solidity-example/blob/main/14_T_Import_1.sol";
+
+contract ExampleImport2{
+    ExampleImport exampleimport = new ExampleImport();
+
+    function getAge() public view returns(uint){
+        return exampleimport.age();
+    }
+
+    function getName() public view returns(string memory){
+        return exampleimport.getName();
+    }
+}
