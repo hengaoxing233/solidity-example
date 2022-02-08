d86f22b664ec30e5ed3352dccc14d0cdd563fc66
hengaoxing233 <76994928+hengaoxing233@users.noreply.github.com>
2022-02-08 11:42:31 +0800

    Create 23_T_Ownable.sol

---
+++ 23_T_Ownable.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Hengaoxing is ERC20, Ownable {
+    constructor() ERC20("hengaoxing", "HGX") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
